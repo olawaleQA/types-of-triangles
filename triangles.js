49db4b1eb93f632046d49945c4a5f9bf436aee1e
olawaleQA <olawaleolowo19@gmail.com>
2024-12-04 13:33:18 +0100

    first commit

---
+++ triangles.js
@@ -0,0 +1,15 @@
+const side1 = 3
+const side2 = 2 
+const side3 = 0 
+
+if(side1==3){
+    console.log('Equilateral triangle')
+} else if(side2==2) {
+    console.log('isosceles triangle')
+} else {
+    console.log('scalene triangle')
+}
+
+// const typesOfTriangles = side1 === "Equilateral" && side2 === "isosceles" && side3 === "scalene"
+
+// console.log(typesOfTriangles)
